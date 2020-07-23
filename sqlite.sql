005e24714e1e17e848da614ecf761071ac578e63
Roman <mister02.rv@gmail.com>
2020-07-23 11:03:20 +0300

    change dir

--- sqlite.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-  user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  email   TEXT NOT NULL UNIQUE,
-  passw   TEXT NOT NULL
-);
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,9 @@
-var sqlite = require('sqlite3')
+const sqlite = require('sqlite3')
 
-let db = new sqlite.Database('../sample.db');
+let db = new sqlite.Database('./sample.db', (err, res) => {
+  if (err) console.log(err)
+  console.log('connected to db')
+});
 
 const findAllUsers = callback => {
   const sql = 'SELECT * FROM users'
@@ -11,5 +14,8 @@ const findUserByEmail = (email, callback) => {
   const sql = 'SELECT * FROM users WHERE email = ?';
   db.get(sql, [email], (err, result) => callback(err, result))
 }
+
+findAllUsers((err, result) => console.log({ result }))
+findUserByEmail('user@gmail.com', (err, res) => console.log({ res }))
 
 module.exports = { findAllUsers, findUserByEmail }
